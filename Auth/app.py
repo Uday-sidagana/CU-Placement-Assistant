e814f3238bfa134956c8aba1ab4fd68b793d652e
Uday-sidagana <uday.sidgana@gmail.com>
2025-03-01 00:15:02 +0530

    Added file.read to file_data and put it in the db

--- Auth/app.py
+++ Auth/app.py
@@ -117,11 +117,12 @@ def registerDetails():
         if existing_user:
             return "UID already registered", 400
 
-
+        file_data = None
         if file:
             filename = f"{uid}.pdf"  # Rename file with UID
             file_path = os.path.join('/Users/macbookair/Desktop/python/CU PLACEMENT ASSISTANT/Auth/uploads', filename)
             file.save(file_path) 
+            file_data = file.read()
 
         user_details = UserDetails(
             uid=uid,
@@ -132,7 +133,7 @@ def registerDetails():
             xii=xii,
             university=university,
             zone=zone,
-            file=file
+            file_data=file
         )
         db.session.add(user_details)
         db.session.commit()
